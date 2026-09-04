5ec785c52a8c3abac490080aae3b31e85ba9ac59
Hatami sugandi <125743957+hatamisg@users.noreply.github.com>
2026-09-04 15:00:23 +0700

    Initial Commit

---
+++ nyawits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nyawits
+//
+//  Created by Hatami Sugandi on 04/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nyawits/nyawitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  nyawitsApp.swift
+//  nyawits
+//
+//  Created by Hatami Sugandi on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct nyawitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
